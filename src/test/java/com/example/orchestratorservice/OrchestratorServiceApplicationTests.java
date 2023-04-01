ea7d4adeba518f0f0e2a7a22852b3473507a9a09
kyomexd <matveev.works@gmail.com>
2023-04-02 02:28:40 +0300

    Orchestrator микросервис (насрал)

---
+++ src/test/java/com/example/orchestratorservice/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orchestratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
