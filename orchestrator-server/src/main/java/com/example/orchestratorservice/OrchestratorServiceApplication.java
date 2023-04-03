9cace1f189ccf4afa5cfeb34363afa292be77885
kyomexd <matveev.works@gmail.com>
2023-04-03 04:10:12 +0300

    old commit (unwanted)

---
+++ orchestrator-server/src/main/java/com/example/orchestratorservice/OrchestratorServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.orchestratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class OrchestratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ orchestrator-server/src/test/java/com/example/orchestratorservice/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.orchestratorservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	public void nasralExampleTest() {
+		Assertions.assertEquals(1, 2);
+	}
+
+}
