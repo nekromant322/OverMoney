9e74f888b2e31e372a71000cee355948e38ff8db
kyomexd <matveev.works@gmail.com>
2023-04-16 13:59:12 +0300

    ELK stack & Slf4j

--- orchestrator_service/src/main/java/com/override/orchestrator_service/controller/rest/OrchestratorController.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/rest/OrchestratorController.java
@@ -1,11 +1,13 @@
 package com.override.orchestrator_service.controller.rest;
 
 import com.override.orchestrator_service.feign.RecognizerFeign;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class OrchestratorController {
 
     @Autowired
@@ -13,6 +15,7 @@ public class OrchestratorController {
 
     @GetMapping("/orchestra")
     public String getOrchestra() {
+        log.info("GET request on /orchestra, test");
         return "Orchestra";
     }
 
--- recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
@@ -1,11 +1,13 @@
 package com.override.recognizer_service.controller.rest;
 
 import com.override.recognizer_service.feign.OrchestratorFeign;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class RecognizerController {
 
     @Autowired
@@ -18,6 +20,7 @@ public class RecognizerController {
 
     @GetMapping("/recognizer")
     public String getRecognizer() {
+        log.info("GET request on /recognizer, test");
         return "Recognizer";
     }
 }
