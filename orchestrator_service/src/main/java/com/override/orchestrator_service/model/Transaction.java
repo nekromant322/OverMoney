94f01d9c8c85c432a4ab5833f2a7d84668847e18
etozhealexis <alexisworksya@yandex.ru>
2023-05-12 20:15:58 +0300

    switched long to uuid

--- orchestrator_service/src/main/java/com/override/orchestrator_service/model/Transaction.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/model/Transaction.java
@@ -3,8 +3,10 @@ package com.override.orchestrator_service.model;
 import brave.internal.Nullable;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name="transactions")
@@ -13,8 +15,10 @@ import javax.persistence.*;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "UUID")
+    @Column(unique = true)
+    private UUID uuid;
 
     @Column
     private String message;
