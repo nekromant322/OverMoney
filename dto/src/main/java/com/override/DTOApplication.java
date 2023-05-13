daf49e0d01cc0014c9793341305464da99578a04
etozhealexis <alexisworksya@yandex.ru>
2023-05-13 21:50:50 +0300

    -dockerfile

--- dto/src/main/java/com/override/DTOApplication.java
+++ dto/src/main/java/com/override/DTOApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DTOServiceApplication {
+public class DTOApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DTOServiceApplication.class, args);
+        SpringApplication.run(DTOApplication.class, args);
     }
 }
