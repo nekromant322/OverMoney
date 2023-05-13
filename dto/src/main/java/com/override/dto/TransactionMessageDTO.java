fdef38726ecb05ecef8fcefe403651f495d68014
etozhealexis <alexisworksya@yandex.ru>
2023-05-14 00:33:10 +0300

    dockerfile

--- dto/src/main/java/com/override/dto/TransactionMessageDTO.java
+++ dto/src/main/java/com/override/dto/TransactionMessageDTO.java
@@ -3,9 +3,11 @@ package com.override.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class TransactionMessageDTO {
     private String message;
