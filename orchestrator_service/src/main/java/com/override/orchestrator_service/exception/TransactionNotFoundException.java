ab894f1150d67b8cd199756f9556948b4f18043e
Vladimir Balakin <vladimirbalakin933@gmail.com>
2023-05-25 19:00:13 +0300

    codestyle

--- orchestrator_service/src/main/java/com/override/orchestrator_service/exception/TransactionNotFoundException.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/exception/TransactionNotFoundException.java
@@ -1,7 +1,5 @@
 package com.override.orchestrator_service.exception;
 
-import org.springframework.data.crossstore.ChangeSetPersister;
-
 public class TransactionNotFoundException extends RuntimeException {
     public TransactionNotFoundException() {
         super();
