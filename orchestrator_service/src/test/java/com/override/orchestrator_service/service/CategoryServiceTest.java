03defb01146d7adc2325dd34c5c28572421dab3c
NikitosikQQ <akmn00@mail.ru>
2023-06-01 18:16:16 +0300

    added unitTests for CategoryMapper, CategoryService, TransactionMapper

--- orchestrator_service/src/test/java/com/override/orchestrator_service/service/CategoryServiceTest.java
+++ orchestrator_service/src/test/java/com/override/orchestrator_service/service/CategoryServiceTest.java
@@ -1,29 +1,37 @@
 package com.override.orchestrator_service.service;
 
+import com.override.dto.CategoryDTO;
 import com.override.orchestrator_service.exception.CategoryNotFoundException;
+import com.override.orchestrator_service.mapper.CategoryMapper;
 import com.override.orchestrator_service.model.Category;
+import com.override.orchestrator_service.model.OverMoneyAccount;
 import com.override.orchestrator_service.repository.CategoryRepository;
+import com.override.orchestrator_service.utils.TestFieldsUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.management.InstanceNotFoundException;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryServiceTest {
 
     @InjectMocks
     private CategoryService categoryService;
-
+    @Mock
+    private CategoryMapper categoryMapper;
     @Mock
     private CategoryRepository categoryRepository;
+    @Mock
+    private OverMoneyAccountService overMoneyAccountService;
 
     @Test
     public void getCategoryByIdThrowExceptionWhenCategoryNotFound() {
@@ -46,5 +54,20 @@ public class CategoryServiceTest {
         Category expectation = categoryService.getCategoryById(category.getId());
 
         assertEquals(expectation, category);
+    }
+
+    @Test
+    public void saveCategoryForAccTest() throws InstanceNotFoundException {
+        final CategoryDTO categoryDTO = TestFieldsUtil.generateTestCategoryDTO();
+        final OverMoneyAccount account = TestFieldsUtil.generateTestAccount();
+        when(overMoneyAccountService.getAccountByUserId(account.getId()))
+                .thenReturn(account);
+
+        when(categoryMapper.mapCategoryDTOToCategory(categoryDTO, account))
+                .thenReturn(TestFieldsUtil.generateTestCategory());
+
+        categoryService.saveCategoryForAcc(TestFieldsUtil.generateTestAccount().getId(), categoryDTO);
+
+        verify(categoryRepository, times(1)).save(any(Category.class));
     }
 }
