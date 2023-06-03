fe5e8665db80fae2273f82f5a1d1d18f437360cc
NikitosikQQ <akmn00@mail.ru>
2023-06-03 17:05:07 +0300

    added functional to merge category

--- orchestrator_service/src/main/java/com/override/orchestrator_service/repository/TransactionRepository.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/repository/TransactionRepository.java
@@ -2,6 +2,7 @@ package com.override.orchestrator_service.repository;
 
 import com.override.orchestrator_service.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,8 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     @Query("SELECT t FROM Transaction t WHERE t.account.id = :id AND t.category.id is null")
     List<Transaction> findAllWithoutCategoriesByAccountId(@Param("id") Long accountId);
+
+    @Modifying
+    @Query("UPDATE Transaction t SET t.category.id = :newCategory WHERE t.category.id= :oldCategory")
+    void updateCategoryId(@Param("oldCategory") Long oldCategoryId, @Param("newCategory") Long newCategoryId);
 }
