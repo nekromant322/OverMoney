e5d30e7a2731f916a8ab044b0a2d7deb6985e3e0
Vladimir Balakin <vladimirbalakin933@gmail.com>
2023-06-06 21:55:37 +0300

    кодстайл

--- orchestrator_service/src/main/java/com/override/orchestrator_service/repository/KeywordRepository.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/repository/KeywordRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword, KeywordId> {
