62410badf883624ebde31e4a9ccdc6614b5b470e
BorisAT <Borisat@gmail.com>
2023-06-20 17:13:16 +0300

    Добавленa асинхронная запись подсказки, исправлено по каментам

--- recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
@@ -5,11 +5,8 @@ import com.override.recognizer_service.feign.OrchestratorFeign;
 import com.override.recognizer_service.service.CategoryRecognizerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
