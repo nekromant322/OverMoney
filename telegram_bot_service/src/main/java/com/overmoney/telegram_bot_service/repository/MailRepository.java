9d56b0e643ffcfd872f732320edb9b2bd8e3fa72
NikitosikQQ <akmn00@mail.ru>
2023-06-22 18:06:24 +0300

    апи для отображения статусов рассылки на фронте

--- telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/repository/MailRepository.java
+++ telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/repository/MailRepository.java
@@ -1,9 +1,26 @@
 package com.overmoney.telegram_bot_service.repository;
 
+import com.overmoney.telegram_bot_service.model.Announce;
 import com.overmoney.telegram_bot_service.model.Mail;
+import com.override.dto.MailDTO;
+import com.override.dto.constants.StatusMailing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MailRepository extends JpaRepository<Mail, Long> {
+
+    @Query("SELECT m FROM Mail m WHERE m.statusMailing= :statusMailing AND m.announce= :announce")
+    List<Mail> findAllMailsByStatusMailing(@Param("statusMailing") StatusMailing statusMailing,
+                                           @Param("announce") Announce announce);
+
+    @Query("SELECT new com.override.dto.MailDTO(m.statusMailing, COUNT(m.statusMailing))\n" +
+            "FROM Mail m \n" +
+            "WHERE m.announce = :announce\n" +
+            "GROUP BY m.statusMailing")
+    List<MailDTO> getCountOfMailsStatusByAnnounce(@Param("announce") Announce announceId);
 }
