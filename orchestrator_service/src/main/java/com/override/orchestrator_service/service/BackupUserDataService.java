1dd5635851427091cd66bef9a0dddd1edade778e
BorisAT <Borisat@gmail.com>
2023-07-06 00:24:51 +0300

    Бека данных в JSON

---
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/service/BackupUserDataService.java
@@ -0,0 +1,28 @@
+package com.override.orchestrator_service.service;
+
+import com.override.dto.BackupUserDataDTO;
+import com.override.dto.CategoryDTO;
+import com.override.dto.TransactionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceNotFoundException;
+import java.util.List;
+
+@Service
+public class BackupUserDataService {
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    public BackupUserDataDTO createBackupUserData(Long telegramId) throws InstanceNotFoundException {
+        List<CategoryDTO> categoryDTOList = categoryService.findCategoriesListByUserId(telegramId);
+        List<TransactionDTO> transactionDTOList = transactionService.findAlltransactionDTOForAcountByUserId(telegramId);
+
+        return BackupUserDataDTO.builder()
+                .categoryDTOList(categoryDTOList)
+                .transactionDTOList(transactionDTOList).build();
+    }
+}
