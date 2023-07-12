26d463fc143e9f6b6eb166359c59a54eb0a4ff21
dementyD <dementyfly@gmail.com>
2023-07-12 18:39:15 +0300

    нужна помощь отправки файла

--- orchestrator_service/src/main/java/com/override/orchestrator_service/controller/rest/SettingsController.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/rest/SettingsController.java
@@ -1,17 +1,25 @@
 package com.override.orchestrator_service.controller.rest;
 
 
+import com.fasterxml.jackson.core.JsonToken;
 import com.override.dto.BackupUserDataDTO;
+import com.override.dto.CategoryDTO;
+import com.override.dto.TransactionDTO;
 import com.override.orchestrator_service.service.BackupUserDataService;
 import com.override.orchestrator_service.util.TelegramUtils;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.boot.configurationprocessor.json.JSONTokener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.management.InstanceNotFoundException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/settings")
@@ -27,5 +35,12 @@ public class SettingsController {
     @GetMapping("/backup")
     public BackupUserDataDTO getUserBackupData(Principal principal) throws InstanceNotFoundException {
         return backupUserDataService.createBackupUserData(telegramUtils.getTelegramId(principal));
+    }
+
+    @PostMapping("/backup/read")
+    public ResponseEntity<HttpStatus> readBackupFile(@RequestParam BackupUserDataDTO backupUserDataDTO) {
+
+
+        return ResponseEntity.ok(HttpStatus.ACCEPTED);
     }
 }
--- orchestrator_service/src/main/resources/static/scripts/setting_scripts.js
+++ orchestrator_service/src/main/resources/static/scripts/setting_scripts.js
@@ -1,19 +1,42 @@
-$("#downloadButton").click(function() {
+let modal = document.getElementById("modal-additional-confirmation");
+
+window.onclick = function closeModalWhenMissClick(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+function closeModalConfirmation() {
+    modal = document.getElementById("modal-additional-confirmation");
+    modal.style.display = "none"
+}
+
+$("#downloadButton").click(function () {
+    saveFile();
+});
+
+$("#readButton").click(function () {
+    modal.style.display = "block";
+    saveFile();
+});
+
+function saveFile() {
     $.ajax({
         url: './settings/backup',
         method: 'GET',
-        success: function(data) {
-            var json = JSON.stringify(data);
-            var blob = new Blob([json], {type: "application/json"});
-            var url  = URL.createObjectURL(blob);
+        async: false,
+        success: function (data) {
+            let json = JSON.stringify(data);
+            let blob = new Blob([json], {type: "application/json"});
+            let url = URL.createObjectURL(blob);
 
-            var a = document.createElement('a');
+            let a = document.createElement('a');
 
             // текущая дата в строку в формате dd.mm.yyyy
-            var date = new Date();
-            var dateString = [
+            let date = new Date();
+            let dateString = [
                 ('0' + date.getDate()).slice(-2),
-                ('0' + (date.getMonth()+1)).slice(-2),
+                ('0' + (date.getMonth() + 1)).slice(-2),
                 date.getFullYear()
             ].join('.');
 
@@ -23,4 +46,87 @@ $("#downloadButton").click(function() {
             a.click();
         }
     });
-});
+}
+
+function readFile(input) {
+    let file = input.files[0];
+    console.log(file);
+
+    let reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        console.log(reader.result)
+        fetch('./settings/backup/read', {
+            method: 'POST',
+            body: JSON.stringify(reader.result),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+    }
+}
+
+// function readFile(input) {
+//     let file = input.files[0];
+//
+//     console.log(file)
+//
+//     let reader = new FileReader();
+//
+//     reader.readAsText(file);
+//
+//     reader.onload = function () {
+//         console.log(reader.result);
+//         $.ajax({
+//             url: './settings/backup/read',
+//             data: JSON.stringify(reader.result),
+//             dataType: 'application/json',
+//             processData: false,
+//             contentType: "application/json; charset=utf8",
+//             type: 'POST',
+//             async: false,
+//             success: function (data) {
+//                 console.log('upload success!')
+//             },
+//             error: function (error) {
+//                 console.log(error)
+//             }
+//         });
+//     };
+// }
+
+
+// //$('#formToFile').submit(function () {
+// function readFile(input) {
+//     let fd = new FormData();
+//     //let selectedFile;
+//     if (window.FormData === undefined) {
+//         alert('В вашем браузере FormData не поддерживается')
+//     } else {
+//         fd.append('file', $('#input')[0].files[0]);
+//         // selectedFile = $('#input').get(0).files[0];
+//     }
+//     $.ajax({
+//         url: './settings/backup/read',
+//         data: fd,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: "application/json; charset=utf8",
+//         type: 'POST',
+//         async: false,
+//         success: function (data) {
+//             console.log('upload success!')
+//         },
+//         error: function (error) {
+//             console.log(error)
+//         }
+//     });
+// //});
+// }
+
+
