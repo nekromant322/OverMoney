bd0179f3870d6e7a70686dc26fc422f97695d002
BorisAT <Borisat@gmail.com>
2023-07-18 15:02:57 +0300

    Авторизация по Кастомному хедеру

--- recognizer_service/src/main/java/com/override/recognizer_service/feign/OrchestratorFeign.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/feign/OrchestratorFeign.java
@@ -1,6 +1,6 @@
 package com.override.recognizer_service.feign;
 
-import com.overmoney.telegram_bot_service.config.FeignConfiguration;
+import com.override.recognizer_service.config.FeignConfiguration;
 import com.override.dto.TransactionDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
