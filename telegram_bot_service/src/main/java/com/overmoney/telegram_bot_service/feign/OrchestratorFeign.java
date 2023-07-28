ba8aee4be24c77f42cd0d34878082e983b4738f3
BorisAT <Borisat@gmail.com>
2023-07-28 15:50:06 +0300

    fix секурити

--- telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/feign/OrchestratorFeign.java
+++ telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/feign/OrchestratorFeign.java
@@ -1,5 +1,6 @@
 package com.overmoney.telegram_bot_service.feign;
 
+import com.overmoney.telegram_bot_service.config.FeignConfiguration;
 import com.override.dto.AccountDataDTO;
 import com.override.dto.ChatMemberDTO;
 import com.override.dto.TransactionMessageDTO;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "orchestrator-service")
+@FeignClient(name = "orchestrator-service", configuration = FeignConfiguration.class)
 public interface OrchestratorFeign {
 
     @PostMapping("/transaction")
