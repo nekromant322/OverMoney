d91941888045036718f7c35bc87f360f605fbfb3
dementyD <dementyfly@gmail.com>
2023-08-02 15:18:09 +0300

    правка обработки исключения

--- orchestrator_service/src/main/java/com/override/orchestrator_service/controller/advice/CommonExceptionHandler.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/advice/CommonExceptionHandler.java
@@ -2,9 +2,9 @@ package com.override.orchestrator_service.controller.advice;
 
 import com.override.dto.CommonErrorDTO;
 import com.override.orchestrator_service.exception.BaseException;
+import com.override.orchestrator_service.exception.RequestSizeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,21 +20,22 @@ public class CommonExceptionHandler {
     @Value("${request.max-size}")
     private String maxSizeRequest;
 
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> test(MaxUploadSizeExceededException e) {
+        return handleException(new RequestSizeException("Размер файла не должен превышать " + maxSizeRequest));
+    }
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<?> handleException(Exception e) {
         log.error(e.getMessage(), e);
         String errorCode = getErrorCode(e);
         int statusCode = getHttpStatusCode(e);
-        String errorMessage = getErrorMessage(e);
-        return ResponseEntity.status(statusCode).body(createDto(errorCode, errorMessage));
+        return ResponseEntity.status(statusCode).body(createDto(errorCode, e.getMessage()));
     }
 
     private String getErrorCode(Exception exception) {
         if (exception instanceof BaseException) {
             return ((BaseException) exception).getErrorCode();
-        }
-        if (exception instanceof MaxUploadSizeExceededException) {
-            return HttpStatus.PAYLOAD_TOO_LARGE.getReasonPhrase();
         }
         return INTERNAL_SERVER_ERROR_CODE;
     }
@@ -43,17 +44,7 @@ public class CommonExceptionHandler {
         if (exception instanceof BaseException) {
             return ((BaseException) exception).getStatusCode();
         }
-        if (exception instanceof MaxUploadSizeExceededException) {
-            return HttpStatus.PAYLOAD_TOO_LARGE.value();
-        }
         return INTERNAL_SERVER_STATUS_CODE;
-    }
-
-    private String getErrorMessage(Exception exception) {
-        if (exception instanceof MaxUploadSizeExceededException) {
-            return "Размер файла не должен превышать " + maxSizeRequest;
-        }
-        return exception.getMessage();
     }
 
     private CommonErrorDTO createDto(String errorCode, String errorMessage) {
