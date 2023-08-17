8a1dfe5cc8445bcc0cb2bf77331cec899cde96f3
Anatolii Marandyuk <shamaneeel881@gmail.com>
2023-08-17 05:17:29 +0300

    lazy trigger build

--- orchestrator_service/src/main/java/com/override/orchestrator_service/OrchestratorServiceApplication.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/OrchestratorServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class OrchestratorServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrchestratorServiceApplication.class, args);
     }
