a31b6a045e24f723ab3e5d5eb4790f86d8378964
Anatolii Marandyuk <shamaneeel881@gmail.com>
2023-08-17 06:34:51 +0300

    Revert "lazy trigger build"
    This reverts commit 8a1dfe5cc8445bcc0cb2bf77331cec899cde96f3.

--- telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/TelegramBotServiceApplication.java
+++ telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/TelegramBotServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class TelegramBotServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotServiceApplication.class, args);
     }
