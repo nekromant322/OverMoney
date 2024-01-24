88eb13955959b3461d98a10ad59fc7b3080f9f69
Anatolii Marandyuk <shamaneeel881@gmail.com>
2024-01-24 03:08:53 +0300

    test envs

--- orchestrator_service/src/main/java/com/override/orchestrator_service/OrchestratorServiceApplication.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/OrchestratorServiceApplication.java
@@ -9,6 +9,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class OrchestratorServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("host");
+        System.out.println(System.getenv("ORCHESTRATOR_DB_HOST"));
+        System.out.println("port");
+        System.out.println(System.getenv("ORCHESTRATOR_DB_PORT"));
+        System.out.println("user");
+        System.out.println(System.getenv("ORCHESTRATOR_DB_USER"));
+        System.out.println("password");
+        System.out.println(System.getenv("ORCHESTRATOR_DB_PASSWORD"));
         SpringApplication.run(OrchestratorServiceApplication.class, args);
     }
 }
