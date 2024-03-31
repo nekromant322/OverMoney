0466be0976ff00ee04d33f56d88fef3d777cac7f
Anatolii Marandyuk <shamaneeel881@gmail.com>
2024-03-31 22:24:59 +0300

    tg bot update

--- telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/feign/OrchestratorFeign.java
+++ telegram_bot_service/src/main/java/com/overmoney/telegram_bot_service/feign/OrchestratorFeign.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(value = "orchestrator", url = "http://sa-orchestrator", configuration = FeignConfiguration.class)
+@FeignClient(value = "orchestrator", url = "http://sa-orchestrator-svc", configuration = FeignConfiguration.class)
 public interface OrchestratorFeign {
 
     @PostMapping("/transaction")
