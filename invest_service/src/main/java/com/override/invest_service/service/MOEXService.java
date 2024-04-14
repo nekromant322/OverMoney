d0de96ae45509e111b3d2d14be67e1855976eaf5
NollReferenceException <itema453@gmail.com>
2024-04-14 23:47:02 +0400

    moex api intagration

--- invest_service/src/main/java/com/override/invest_service/service/MOEXService.java
+++ invest_service/src/main/java/com/override/invest_service/service/MOEXService.java
@@ -1,72 +1,63 @@
 package com.override.invest_service.service;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class MOEXService {
-    private final Map<String, Double> tickerToWeight = new HashMap<>();
-    //todo
-    // стянуть веса индекса мосбиржи с картинки  https://informer.moex.com/ru/index/constituents-IMOEX-20230721.gif?page=0
-    // можно просто совать актуальную дату в имя файла
+
+    private final String IMOEX_JSON_STRING_URL = "https://iss.moex.com/iss/statistics/engines/stock/markets/index/analytics/imoex.json?limit=100";
+    private final String IMOEX_DATA_FILENAME = "IMOEX_Data-invest.json";
+    private final Long DATA_AGE_LIMIT = 1L;
+    private final int SECIDS_INDEX = 4;
+    private final int WEIGHT_INDEX = 5;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
+    private Map<String, Double> tickerToWeight = new HashMap<>();
+
 
     @PostConstruct
     public void init() {
-        tickerToWeight.put("AFKS", 0.58d);
-        tickerToWeight.put("AFLT", 0.53d);
-        tickerToWeight.put("AGRO", 0.86d);
-        tickerToWeight.put("ALRS", 1.28d);
-        tickerToWeight.put("CBOM", 0.74d);
-        tickerToWeight.put("CHMF", 2.22d);
-        tickerToWeight.put("ENPG", 0.41d);
-        tickerToWeight.put("FEES", 0.49d);
-        tickerToWeight.put("FIVE", 0.97d);
-        tickerToWeight.put("FLOT", 0.62d);
-        tickerToWeight.put("GAZP", 11.14d);
-        tickerToWeight.put("GLTR", 0.24d);
-        tickerToWeight.put("GMKN", 6.05d);
-        tickerToWeight.put("HYDR", 0.26d);
-        tickerToWeight.put("IRAO", 1.55d);
-        tickerToWeight.put("LKOH", 14.47d);
-        tickerToWeight.put("MAGN", 1.28d);
-        tickerToWeight.put("MGNT", 1.97d);
-        tickerToWeight.put("MOEX", 1.02d);
-        tickerToWeight.put("MSNG", 0.38d);
-        tickerToWeight.put("MTLR", 0.53d);
-        tickerToWeight.put("MTLRP",0.38d);
-        tickerToWeight.put("MTSS", 1.22d);
-        tickerToWeight.put("NLMK", 1.74d);
-        tickerToWeight.put("NVTK", 3.06d);
-        tickerToWeight.put("OZON", 0.96d);
-        tickerToWeight.put("PHOR", 0.8d);
-        tickerToWeight.put("PIKK", 2.06d);
-        tickerToWeight.put("PLZL", 2.34d);
-        tickerToWeight.put("POLY", 0.21d);
-        tickerToWeight.put("POSI", 0.35d);
-        tickerToWeight.put("ROSN", 2.33d);
-        tickerToWeight.put("RTKM", 1.06d);
-        tickerToWeight.put("RUAL", 1.66d);
-        tickerToWeight.put("SBER", 13.11d);
-        tickerToWeight.put("SBERP", 2.53d);
-        tickerToWeight.put("SELG", 0.29d);
-        tickerToWeight.put("SGZH", 0.24d);
-        tickerToWeight.put("SMLT", 0.35d);
-        tickerToWeight.put("SNGS", 3.13d);
-        tickerToWeight.put("SNGSP", 2.98d);
-        tickerToWeight.put("TATN", 5.73d);
-        tickerToWeight.put("TATNP", 1.05d);
-        tickerToWeight.put("TCSG", 0.62d);
-        tickerToWeight.put("TRNFP", 0.66d);
-        tickerToWeight.put("UPRO", 0.31d);
-        tickerToWeight.put("VKCO", 0.41d);
-        tickerToWeight.put("VTBR", 0.94d);
-        tickerToWeight.put("YNDX", 1.9d);
+        try {
+            File jsonDataFile = new File(IMOEX_DATA_FILENAME);
+            JsonNode jsonNode;
+
+            if (jsonDataFile.exists() && checkJsonByFreshness(jsonDataFile)) {
+
+                jsonNode = objectMapper.readTree(jsonDataFile);
+            } else {
+                URL imoexJsonURL = new URL(IMOEX_JSON_STRING_URL);
+                jsonNode = objectMapper.readTree(imoexJsonURL);
+                objectWriter.writeValue(new File(IMOEX_DATA_FILENAME), jsonNode);
+            }
+
+            for (JsonNode jn : jsonNode.get("analytics").get("data")) {
+                tickerToWeight.put(jn.get(SECIDS_INDEX).asText(), jn.get(WEIGHT_INDEX).asDouble());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Map<String, Double> getTickerToWeight() {
         return tickerToWeight;
+    }
+
+    private boolean checkJsonByFreshness(File file) {
+        return (LocalDate.now().toEpochDay() - TimeUnit.MILLISECONDS.toDays(file.lastModified()) <= DATA_AGE_LIMIT);
     }
 }
