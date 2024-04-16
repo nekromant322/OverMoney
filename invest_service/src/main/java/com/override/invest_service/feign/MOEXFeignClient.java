5f6a2a540cb12daf626c2d985250e6838b775ff7
NollReferenceException <itema453@gmail.com>
2024-04-16 22:21:28 +0400

    добавлен MOEXFeignClient и необходимые бины

---
+++ invest_service/src/main/java/com/override/invest_service/feign/MOEXFeignClient.java
@@ -0,0 +1,15 @@
+package com.override.invest_service.feign;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(value = "moex", url = "${moex.index-url-data}")
+public interface MOEXFeignClient {
+
+    @GetMapping("/imoex.json")
+    ResponseEntity<JsonNode> getIndexIMOEX(@RequestParam("limit") int limit);
+}
--- invest_service/src/main/java/com/override/invest_service/service/MOEXService.java
+++ invest_service/src/main/java/com/override/invest_service/service/MOEXService.java
@@ -1,16 +1,17 @@
 package com.override.invest_service.service;
 
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.override.invest_service.feign.MOEXFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,41 +20,47 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class MOEXService {
 
-    private final String IMOEX_JSON_STRING_URL = "https://siss.moex.com/iss/statistics/engines/stock/markets/index/analytics/imoex.json?limit=100";
     private final String IMOEX_DATA_FILENAME = "IMOEX_Data-invest.json";
     private final File jsonDataFile = new File(IMOEX_DATA_FILENAME);
     private final Long DATA_AGE_LIMIT = 1L;
     private final int SECIDS_INDEX = 4;
     private final int WEIGHT_INDEX = 5;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
+    @Autowired
+    private MOEXFeignClient MOEXFeignClient;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ObjectWriter objectWriter;
+
     private Map<String, Double> tickerToWeight = new HashMap<>();
 
-    private JsonNode jsonNode;
 
     @PostConstruct
     public void init() {
         try {
+            JsonNode jsonNode;
+
             if (jsonDataFile.exists() && checkJsonByFreshness(jsonDataFile)) {
                 jsonNode = readDataFromFile(jsonDataFile);
             } else {
-                URL imoexJsonURL = new URL(IMOEX_JSON_STRING_URL);
-                jsonNode = objectMapper.readTree(imoexJsonURL);
-                objectWriter.writeValue(new File(IMOEX_DATA_FILENAME), jsonNode);
-            }
-        } catch (MalformedURLException e) {
-            RuntimeException urlException = new RuntimeException
-                    ("проблемы с получением данных с url: [" + IMOEX_JSON_STRING_URL, e);
-            urlException.printStackTrace();
+                ResponseEntity<JsonNode> response = MOEXFeignClient.getIndexIMOEX(100);
 
-            jsonNode = readDataFromFile(jsonDataFile);
+                if (response.getStatusCode() == HttpStatus.OK) {
+                    jsonNode = response.getBody();
+                    objectWriter.writeValue(jsonDataFile, jsonNode);
+                } else {
+                    jsonNode = readDataFromFile(jsonDataFile);
+                }
+            }
+
+            for (JsonNode jn : jsonNode.get("analytics").get("data")) {
+                tickerToWeight.put(jn.get(SECIDS_INDEX).asText(), jn.get(WEIGHT_INDEX).asDouble());
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-        for (JsonNode jn : jsonNode.get("analytics").get("data")) {
-            tickerToWeight.put(jn.get(SECIDS_INDEX).asText(), jn.get(WEIGHT_INDEX).asDouble());
         }
     }
 
