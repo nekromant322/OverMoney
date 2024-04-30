cc3aede34f9db495c0893b72b86420ee492d73ba
NikoTiN161 <dmitrii.asenenko@gmail.com>
2024-04-30 17:01:32 +0300

    add env, return back homepage in package.json, change config nginx

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import './App.scss';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Navigate,  Route, Routes, useLocation } from 'react-router-dom';
 import Overmoney from '../Overmoney/Overmoney';
 import History from '../History/History';
 import Analytics from '../Analytics/Analytics';
@@ -9,18 +9,18 @@ import Settings from '../Settings/Settings';
 import Header from '../Header/Header';
 
 const App: FC = () => {
-
+  const url = process.env.REACT_APP_PATH_TO_HOST || '/front';
   return (
     <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" Component={Overmoney} />
-            <Route path="/overmoney" Component={Overmoney} />
-            <Route path="/history" Component={History} />
-            <Route path="/analytics" Component={Analytics} />
-            <Route path="/micromanagement" Component={Micromanagement} />
-            <Route path="/settings" Component={Settings} />
-          </Routes>
+      <Header />
+      <Routes>
+        <Route path={`${url}/overmoney`} Component={Overmoney} />
+        <Route path={`${url}/history`} Component={History} />
+        <Route path={`${url}/analytics`} Component={Analytics} />
+        <Route path={`${url}/micromanagement`} Component={Micromanagement} />
+        <Route path={`${url}/settings`} Component={Settings} />
+        <Route path={`${url}/*`} element={<Navigate to={`${url}/overmoney`} />} />
+      </Routes>
     </BrowserRouter>
   );
 }
