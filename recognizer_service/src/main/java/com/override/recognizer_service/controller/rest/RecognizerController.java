caed22cef18f1a6e319a07f2c998cdc779470101
Elminst3r <judge.konstantin@gmail.com>
2024-06-23 20:08:49 +0300

    Удалён семпловый метод getRecognizer

--- recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/RecognizerController.java
@@ -16,17 +16,9 @@ public class RecognizerController {
     @Autowired
     private CategoryRecognizerService categoryRecognizerService;
 
-    @GetMapping("/recognizer")
-    public String getRecognizer() {
-        log.info("GET request on /recognizer, test");
-        return "Recognizer";
-    }
-
 
     @PostMapping("/recognizer/category/suggested")
-    public void recognizeCategory(@RequestParam(name = "message") String message,
-                                  @RequestParam(name = "transactionId") UUID transactionId,
-                                  @RequestBody List<CategoryDTO> categories) {
+    public void recognizeCategory(@RequestParam(name = "message") String message, @RequestParam(name = "transactionId") UUID transactionId, @RequestBody List<CategoryDTO> categories) {
         categoryRecognizerService.sendTransactionWithSuggestedCategory(message, categories, transactionId);
     }
 
