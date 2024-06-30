c2ba5cc542ed083494cdbf3f6b0a66f1a7ff75c4
Anatolii Marandyuk <shamaneeel881@gmail.com>
2024-06-30 15:56:51 +0300

    logs for voice recognition

--- recognizer_service/src/main/java/com/override/recognizer_service/service/voice/VoiceDTORecognitionServiceImplGoAudioRecognizer.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/service/voice/VoiceDTORecognitionServiceImplGoAudioRecognizer.java
@@ -40,6 +40,8 @@ public class VoiceDTORecognitionServiceImplGoAudioRecognizer implements VoiceDTO
         try(OutputStream outputStream = connection.getOutputStream()) {
             byte[] input = mapper.writeValueAsBytes(request);
             outputStream.write(input, 0, input.length);
+        } catch (Exception e) {
+            log.error("Error sending voice message to wit ai go proxy, id= " + request.getId(), e);
         }
 
         try(BufferedReader br = new BufferedReader(
@@ -50,6 +52,8 @@ public class VoiceDTORecognitionServiceImplGoAudioRecognizer implements VoiceDTO
                 response.append(responseLine.trim());
             }
             decryptedMessageAsJSON = response.toString();
+        } catch (Exception e) {
+            log.error("Error processing response from wit ai go proxy, id= " + request.getId(), e);
         }
         log.info("Got json result from wit.ai go proxy " + decryptedMessageAsJSON);
         return mapper.readValue(decryptedMessageAsJSON, AudioRecognizerGoResponseDTO.class).getText();
