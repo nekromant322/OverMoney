b573dc0f75d067c34bf2e9108b9218ddd159b059
NikoTiN161 <dmitrii.asenenko@gmail.com>
2024-08-11 15:45:47 +0300

    fix: перенес получение имени бота ТГ в компонет виджета

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
+import React, { FC, useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import TelegramLoginButton from '../components/TelegramLoginButton';
 import { useNavigate } from 'react-router-dom';
@@ -10,19 +10,10 @@ const LoginPage: FC = () => {
     const url = process.env.REACT_APP_PATH_TO_HOST || '/front'
     const baseUrl = process.env.REACT_APP_BASE_URL || 'https://overmoney.tech'
 
-    const [botName, setBotName] = useState('')
     const { setAuthenticated } = useContext(AuthContext)
 
     const navigate = useNavigate()
 
-    useEffect(() => {
-        axios.get(`${baseUrl}/login/bot-login`)
-            .then(response => {
-                setBotName(response.data)
-                console.log(response.data)
-            })
-            .catch(error => console.log(error))
-    }, [])
     // Обработка данных пользователя после аутентификации через Telegram Login Widget
     const handleUserAuth = (user: IUser) => {
         console.log(user); 
@@ -37,8 +28,7 @@ const LoginPage: FC = () => {
     };
     return (
         <Container className="d-flex align-items-center justify-content-center mt-5">
-            <TelegramLoginButton botName={botName} dataOnauth={handleUserAuth} />
-            {/* <TelegramLoginButton botName="testSignin_bot" dataOnauth={handleUserAuth} /> */}
+            <TelegramLoginButton dataOnauth={handleUserAuth} />
         </Container>
     );
 };
