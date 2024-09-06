9bc00eaf2ce03a1a713d28dc31a2a51e5c282dcf
annov4 <137298843+annov4@users.noreply.github.com>
2024-09-06 18:29:42 +0300

    checkstyle corrected

--- recognizer_service/src/main/java/com/override/recognizer_service/service/CategoryRecognizerService.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/service/CategoryRecognizerService.java
@@ -40,9 +40,9 @@ public class CategoryRecognizerService {
         float[] maxLevenshteinDistance = {0};
         categories.forEach(c -> {
             c.getKeywords().add(
-                    KeywordIdDTO.builder()
-                            .name(c.getName())
-                            .build());
+                KeywordIdDTO.builder()
+                    .name(c.getName())
+                    .build());
         });
         categories.forEach(c -> {
             c.getKeywords().forEach(k -> {
@@ -58,21 +58,19 @@ public class CategoryRecognizerService {
 
     public void sendTransactionWithSuggestedCategory(String message, List<CategoryDTO> categories, UUID transactionId) {
         CategoryDTO suggestedCategory = recognizeCategory(message, categories);
-        Long suggestedCategoryId = suggestedCategory != null && calculateAccuracy(suggestedCategory, message)
-                            >= minAccuracy ? suggestedCategory.getId() : null;
         float accuracy = suggestedCategory != null ? calculateAccuracy(suggestedCategory, message) : 0.0f;
         TransactionDTO transactionDTO = TransactionDTO.builder()
-                            .accuracy(accuracy)
-                            .id(transactionId)
-                            .suggestedCategoryId(suggestedCategoryId)
-                            .build();
+            .accuracy(accuracy)
+            .id(transactionId)
+            .suggestedCategoryId(suggestedCategoryId)
+            .build();
         orchestratorFeign.editTransaction(transactionDTO);
     }
 
     private float calculateAccuracy(CategoryDTO category, String message) {
         return category.getKeywords().stream()
-                            .map(k -> calculateLevenshteinDistance(message, k.getName()))
-                            .max(Float::compare)
-                            .orElse(0.0f);
+            .map(k -> calculateLevenshteinDistance(message, k.getName()))
+            .max(Float::compare)
+            .orElse(0.0f);
     }
 }
