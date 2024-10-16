d11d8312a5c193e37a49f4003c98038f05f39512
Anatolii Marandyuk <shamaneeel881@gmail.com>
2024-10-16 14:22:01 +0300

    add logs suggestion

--- orchestrator_service/src/main/java/com/override/orchestrator_service/service/SuggestionService.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/service/SuggestionService.java
@@ -4,12 +4,14 @@ import com.override.dto.TransactionDTO;
 import com.override.orchestrator_service.model.Suggestion;
 import com.override.orchestrator_service.model.Transaction;
 import com.override.orchestrator_service.repository.SuggestionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class SuggestionService {
 
     @Autowired
@@ -33,7 +35,7 @@ public class SuggestionService {
     public void estimateSuggestionCorrectness(UUID transactionId, Long categoryId) {
         Transaction transaction = transactionService.getTransactionById(transactionId);
         Suggestion suggestion = transaction.getSuggestion();
-
+        log.info("estimateSuggestionCorrectness: transaction id=" + transactionId + " suggestion= " + suggestion.toString());
         suggestion.setIsCorrect(categoryId.equals(suggestion.getSuggestedCategoryId()));
         suggestionRepository.save(suggestion);
     }
