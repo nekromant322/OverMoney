ced0601d910fe8210ed0dc17a5245ac75936b7b2
VEIIEV <62066130+VEIIEV@users.noreply.github.com>
2025-03-29 01:27:47 +0400

    private
    Co-authored-by: OverrideTech <43813324+nekromant322@users.noreply.github.com>

--- recognizer_service/src/main/java/com/override/recognizer_service/service/DeepSeekBalanceService.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/service/DeepSeekBalanceService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class DeepSeekBalanceService {
 
     @Autowired
-    DeepSeekFeignClient client;
+    private DeepSeekFeignClient client;
 
     @Value("${deepseek.auth-token}")
     private String authToken;
