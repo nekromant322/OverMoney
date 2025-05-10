cd327c0942d463bb5bec9be8e8007ecedf86210f
Alex Kolosov <megokolos@gmail.com>
2025-05-10 21:29:20 +0300

    make core

---
+++ payment_service/src/main/java/com/override/payment_service/PaymentServiceApplication.java
@@ -0,0 +1,17 @@
+package com.override.payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
