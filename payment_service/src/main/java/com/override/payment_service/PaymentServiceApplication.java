ed35f5c2a51d9ede8abff2f88163070c0017012b
Alex Kolosov <megokolos@gmail.com>
2025-05-11 15:32:08 +0300

    add test method

--- payment_service/src/main/java/com/override/payment_service/PaymentServiceApplication.java
+++ payment_service/src/main/java/com/override/payment_service/PaymentServiceApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 @EnableFeignClients
 public class PaymentServiceApplication {
--- payment_service/src/test/java/com/override/payment_service/PaymentServiceApplicationTest.java
+++ payment_service/src/test/java/com/override/payment_service/PaymentServiceApplicationTest.java
@@ -1,5 +1,6 @@
-package com.override;
+package com.override.payment_service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
