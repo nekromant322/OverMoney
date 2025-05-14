c44bfb20a620301f0ce72e075b9ed75e8bb31779
bahertylop <levichankin@gmail.com>
2025-05-15 00:11:29 +0300

    OV-342: скопировал лендинг с ветки фронтенда

---
+++ frontend_landing/src/main.js
@@ -0,0 +1,39 @@
+const LOGIN_URL = `${import.meta.env.VITE_BASE_URL}/auth/login`;
+
+window.addEventListener('load', async () => {
+  embedTelegramWidget()
+})
+
+window.onTelegramAuth = async (user) => {
+  console.log(LOGIN_URL);
+  const response = await fetch({
+    method: 'POST',
+    url: LOGIN_URL,
+    data: JSON.stringify(user),
+    contentType: "application/json; charset=utf8",
+  });
+
+  if (response.status !== 200) {
+    console.error('Authorization failed');
+    return;
+  }
+
+  window.location.href = `${import.meta.env.VITE_APP_URL}`;
+}
+
+function embedTelegramWidget() {
+  const script = document.createElement('script');
+
+  script.async = true;
+  script.setAttribute('src', `https://telegram.org/js/telegram-widget.js?${import.meta.env.VITE_TELEGRAM_WIDGET_VERSION}`);
+  script.setAttribute('data-telegram-login', import.meta.env.VITE_TELEGRAM_BOT_NAME);
+  script.setAttribute("data-size", "medium");
+  script.setAttribute("data-onauth", "onTelegramAuth(user)");
+  script.setAttribute("data-request-access", "write");
+  script.setAttribute('data-lang', 'ru');
+  script.setAttribute('data-radius', '6');
+  script.setAttribute('data-userpic', 'true');
+
+  const element = document.getElementById('login-button');
+  element.appendChild(script);
+}
---
+++ frontend_landing/vite.config.js
@@ -0,0 +1,13 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      ViteImageOptimizer(),
+    ],
+    server: {
+      allowedHosts: true
+    }
+  };
+});
