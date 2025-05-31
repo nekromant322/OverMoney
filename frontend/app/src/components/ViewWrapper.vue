57f62c3803ec0bf1834d0c98d625c5c311bf2979
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2025-06-01 05:10:48 +0700

    Add login view (partially completed)

---
+++ frontend/app/src/components/ViewWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: #0D1117;
+  padding: 0 32px;
+  margin: 0 auto;
+  min-height: 100vh;
+}
+</style>
