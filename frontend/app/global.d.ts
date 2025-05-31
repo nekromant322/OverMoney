12021f383176bf620d45c8cc1dbde6b2763956ad
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2025-06-01 05:10:48 +0700

    Add login view (partially completed)

---
+++ frontend/app/global.d.ts
@@ -0,0 +1,17 @@
+interface TelegramUser {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  photo_url?: string;
+  auth_date: number;
+  hash: string;
+}
+
+declare global {
+  interface Window {
+    onTelegramAuth: (user: TelegramUser) => void;
+  }
+}
+
+export { TelegramUser };
