287f58c1aa4b2a17c259092f896676d995da5f0f
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2025-06-04 18:29:32 +0700

    Add some header elements

---
+++ frontend/app/src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import logo from '@/assets/images/logo.svg';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header class="header">
+      <RouterLink to="/">
+        <img class="logo" :src="logo" width="32" height="32" alt="app logo">
+      </RouterLink>
+      <button class="profile-button">
+        <img class="avatar" width="32" height="32" alt="user avatar">
+      </button>
+    </header>
+</template>
+
+<style scoped>
+.header {
+  padding: 14px 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+}
+
+.logo {
+  display: block;
+}
+
+.profile-button {
+  background-color: transparent;
+  border: none;
+}
+
+.avatar {
+  display: block;
+}
+</style>
--- frontend/app/src/views/OperationsView.vue
+++ frontend/app/src/views/OperationsView.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue';
+import AppHeader from '@/components/AppHeader.vue';
+import ViewWrapper from '@/components/ViewWrapper.vue';
+import { ref, onMounted } from 'vue';
 
-  const data = ref(null);
+const data = ref(null);
 
-  onMounted(async () => {
-    try {
-      const response = await fetch('/api/transactions', {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-      },
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      data.value = await response.json();
-    } catch (error) {
-      console.error('Error fetching data:', error);
+onMounted(async () => {
+  try {
+    const response = await fetch('/api/transactions', {
+      method: 'GET',
+      headers: {
+      'Content-Type': 'application/json',
+    },
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  });
+    data.value = await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
 </script>
 
 <template>
-  <main>
-    Operations view
-  </main>
+  <ViewWrapper>
+    <AppHeader></AppHeader>
+  </ViewWrapper>
 </template>
