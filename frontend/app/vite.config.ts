85cadbe366942f78bafef47b9818aadf6628fa17
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2025-06-04 09:05:03 +0700

    Make app available from external hosts only in dev mode

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,27 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/transactions': {
-        target: 'https://overmoneytest.online',
-        changeOrigin: true,
-        secure: false,
-      }
+    server: {
+      allowedHosts: [env.VITE_ALLOWED_HOSTS]
     }
-  }
-})
+  });
+};
