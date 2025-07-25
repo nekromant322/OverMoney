3de4af6e9b9bd4ea57150b8eeeef0f8e239d5213
vkusnyjchaj <vkusnyjchaj@gmail.com>
2025-07-25 12:23:19 +0700

    Add expenses/income tabs (partially completed)

---
+++ frontend/app/src/components/ExpensesIncomeTab.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div :class="$style.tab">
+    <ul :class="$style.list">
+      <li v-for="index in 100" :key="index" :class="$style.item">
+        <div :class="$style.category">Бизнес</div>
+        <div :class="$style.price">- 120 840</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style module>
+.tab {
+  overflow: auto;
+  margin-top: 16px;
+  padding-right: 24px;
+  scrollbar-color: #424242 #686868;
+  box-sizing: border-box;
+  height: Calc(100% - 80px);
+}
+
+.list {
+  padding: 16px 0;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.category {
+  font-family: 'Noto Sans', Arial, Helvetica, sans-serif;
+  font-size: 12px;
+  color: #C9D1D9;
+  line-height: 40px;
+}
+
+.price {
+  font-family: 'Noto Sans', Arial, Helvetica, sans-serif;
+  font-size: 12px;
+  color: #FF918B;
+  line-height: 40px;
+}
+</style>
--- frontend/app/src/components/OperationsTab.vue
+++ frontend/app/src/components/OperationsTab.vue
@@ -2,48 +2,41 @@
 import AppIcon from '@/components/AppIcon.vue';
 import AppInput from '@/components/AppInput.vue';
 import SearchIcon from '@/assets/images/Search.svg';
-import { defineAsyncComponent, ref } from 'vue';
+import { ref } from 'vue';
 import TextTabButton from './TextTabButton.vue';
 import TimeFilter, { TimePeriod } from './TimeFilter.vue';
-
-const ExpensesTab = defineAsyncComponent(() =>
-  import('@/components/ExpensesTab.vue')
-);
-
-const IncomeTab = defineAsyncComponent(() =>
-  import('@/components/IncomeTab.vue')
-);
+import ExpensesIncomeTab from './ExpensesIncomeTab.vue';
 
 const category = ref('');
 const period = ref(TimePeriod.M);
 const activeTab = ref(0);
 const tabs = [
-  { id: 'expenses', title: 'Расходы', component: ExpensesTab },
-  { id: 'income', title: 'Доходы', component: IncomeTab }
+  { id: 'expenses', title: 'Расходы' },
+  { id: 'income', title: 'Доходы' }
 ];
 </script>
 
 <template>
   <div :class="$style.tab">
     <aside :class="$style.categories">
-      <AppInput v-model="category" placeholder="Найти категорию...">
-        <AppIcon :src="SearchIcon" alt="search" />
-      </AppInput>
-      <div :class="$style.filters">
-        <div>
-          <TextTabButton 
-            v-for="(tab, index) in tabs" 
-            :key="tab.id" 
-            :title="tab.title" 
-            :class="$style.textTabButton"
-            :selected="activeTab === index"
-            @click="activeTab = index" />
+      <div :class="$style.settings">
+        <AppInput v-model="category" placeholder="Найти категорию...">
+          <AppIcon :src="SearchIcon" alt="search" />
+        </AppInput>
+        <div :class="$style.filters">
+          <div>
+            <TextTabButton 
+              v-for="(tab, index) in tabs" 
+              :key="tab.id" 
+              :title="tab.title" 
+              :class="$style.textTabButton"
+              :selected="activeTab === index"
+              @click="activeTab = index" />
+          </div>
+          <TimeFilter :class="$style.timeFilter" v-model="period" />
         </div>
-        <TimeFilter :class="$style.timeFilter" v-model="period" />
       </div>
-      <KeepAlive>
-        <component :is="tabs[activeTab].component" />
-      </KeepAlive>
+      <ExpensesIncomeTab />
     </aside>
     <main :class="$style.operations">
       Operations
@@ -60,8 +53,12 @@ const tabs = [
 }
 
 .categories {
-  padding: 24px 24px 24px 0;
+  padding: 24px 0;
   border-right: 1px solid #30363D;
+}
+
+.settings {
+  padding-right: 24px;
 }
 
 .filters {
