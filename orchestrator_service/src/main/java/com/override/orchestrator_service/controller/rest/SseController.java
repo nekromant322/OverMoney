fbc38e1d5e86e1a4c71a830144f1b6881c6d53fd
ninelive <chesnokov_sergey9@mail.ru>
2025-10-06 15:57:37 +0300

    add SSE classes for open connection and send messages

---
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/rest/SseController.java
@@ -0,0 +1,56 @@
+package com.override.orchestrator_service.controller.rest;
+
+import com.override.orchestrator_service.config.jwt.JwtAuthentication;
+import com.override.orchestrator_service.kafka.consumerproducer.SseProducer;
+import com.override.orchestrator_service.model.Transaction;
+import com.override.orchestrator_service.model.User;
+import com.override.orchestrator_service.service.SseService;
+import com.override.orchestrator_service.service.TransactionService;
+import com.override.orchestrator_service.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/sse")
+@Tag(name = "Контроллер SSE соединений", description = "Операции, связанные с Server Sent Events соединениями")
+public class SseController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SseService sseService;
+    @Autowired
+    private SseProducer sseProducer;
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping(value = "/open-stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent> openStream(Authentication authentication) {
+        if (authentication == null && !(authentication.getPrincipal() instanceof JwtAuthentication)) {
+            return Flux.error(new RuntimeException("Not authenticated"));
+        }
+        String username = (String) authentication.getPrincipal();
+        User user = userService.getUserByUsername(username);
+        return Flux.create(fluxSink -> {
+            log.info("create subscription for " + username);
+            sseService.addSubscription(user, fluxSink);
+            sseService.sendInitData(user.getId(), fluxSink);
+        });
+    }
+
+    @PostMapping("/uncategorized-notify")
+    public ResponseEntity<Void> handleUncategorizedTransaction(@RequestParam UUID transactionId) {
+        Transaction transaction = transactionService.getTransactionById(transactionId);
+        sseProducer.sendMessage(transaction.getTelegramUserId());
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/service/SseService.java
@@ -0,0 +1,55 @@
+package com.override.orchestrator_service.service;
+
+
+import com.override.dto.TransactionDTO;
+import com.override.orchestrator_service.mapper.TransactionMapper;
+import com.override.orchestrator_service.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.FluxSink;
+
+import javax.management.InstanceNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class SseService {
+    @Autowired
+    private TransactionService transactionService;
+    @Autowired
+    private TransactionMapper transactionMapper;
+
+    private Map<Long, FluxSink<ServerSentEvent>> subscriptions = new ConcurrentHashMap<>();
+
+    public void addSubscription(User user, FluxSink<ServerSentEvent> fluxSink) {
+        fluxSink.onCancel(() -> {
+                    subscriptions.remove(user.getId());
+                    log.info("subscription " + user.getUsername() + " was closed");
+                }
+        );
+        subscriptions.put(user.getId(), fluxSink);
+    }
+
+    public void sendInitData(Long id, FluxSink<ServerSentEvent> fluxSink) {
+        try {
+            List<TransactionDTO> transactionsDTO = transactionService.findTransactionsListByUserIdWithoutCategories(id).stream()
+                    .map(transaction -> transactionMapper.mapTransactionToDTO(transaction))
+                    .collect(Collectors.toList());
+            ServerSentEvent<List<TransactionDTO>> event = ServerSentEvent.builder(transactionsDTO).build();
+            fluxSink.next(event);
+        } catch (InstanceNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendNewTransactionsToUser(Long id) {
+        if (subscriptions.containsKey(id)) {
+            sendInitData(id, subscriptions.get(id));
+        }
+    }
+}
