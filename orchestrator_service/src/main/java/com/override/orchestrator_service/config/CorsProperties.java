f47ce103e3513c91ab19230eb0e6ea762fc7f98e
ninelive <chesnokov_sergey9@mail.ru>
2025-10-06 17:31:44 +0300

    add cors settings in SecurityConfig for two spring profiles

---
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/config/CorsProperties.java
@@ -0,0 +1,22 @@
+package com.override.orchestrator_service.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "app.cors")
+public class CorsProperties {
+    private List<String> allowedOrigins = new ArrayList<>();
+
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
