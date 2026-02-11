92bbd767163db45ae5af96fc5c55ba11abdab967
argunovsd <argunovsd@yandex.ru>
2026-02-11 18:02:37 +0300

    Clean PR

--- orchestrator_service/src/main/java/com/override/orchestrator_service/controller/UserViewController.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/UserViewController.java
@@ -12,14 +12,14 @@ public class UserViewController {
         return new RedirectView("/overmoney");
     }
 
-    @GetMapping("/offer")
-    public String getOffer() {
-        return "offer";
-    }
-
     @GetMapping("/overmoney")
     public String getMainPage() {
         return "overmoney";
+    }
+
+    @GetMapping("/offer")
+    public String getOffer() {
+        return "offer";
     }
 
     @GetMapping("/history")
