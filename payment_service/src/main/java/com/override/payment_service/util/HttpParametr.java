b5b2685edeb4c8120af05cb0c91f2fdfe6430cbf
heart <sergeyargunov8@gmail.com>
2026-02-14 19:31:53 +0300

    triggerQA

--- payment_service/src/main/java/com/override/payment_service/util/HttpParametr.java
+++ payment_service/src/main/java/com/override/payment_service/util/HttpParametr.java
@@ -9,6 +9,7 @@ import java.util.Map;
 public class HttpParametr {
     private Long invoiceId;
     private BigDecimal outSum;
+
     private String httpSignature;
 
     public HttpParametr(Map<String, String> mapOfParametrs){
