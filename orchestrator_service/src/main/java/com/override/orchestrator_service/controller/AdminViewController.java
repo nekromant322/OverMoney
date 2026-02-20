c38d3abba499df1d8f17d4cff3350a4b0ff1f710
heart <sergeyargunov8@gmail.com>
2026-02-20 16:23:12 +0300

    триггер сервисов для argo

--- orchestrator_service/src/main/java/com/override/orchestrator_service/controller/AdminViewController.java
+++ orchestrator_service/src/main/java/com/override/orchestrator_service/controller/AdminViewController.java
@@ -2,6 +2,7 @@ package com.override.orchestrator_service.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
