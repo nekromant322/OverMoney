bda85ca51ea4e3f1c9c3f6e5ec32bec8aaf46805
heart <sergeyargunov8@gmail.com>
2026-02-23 20:29:11 +0300

    Rollback kafka config

--- recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/DeepSeekMetaInfoController.java
+++ recognizer_service/src/main/java/com/override/recognizer_service/controller/rest/DeepSeekMetaInfoController.java
@@ -4,7 +4,6 @@ import com.override.recognizer_service.service.DeepSeekBalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
