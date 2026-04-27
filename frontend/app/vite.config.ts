9c13d39db16a07a71e53126f8ebc01db5107942a
Anatolii Marandyuk <shamaneeel881@gmail.com>
2026-04-27 21:14:35 +0300

    front urls fix

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/front/' : '/',
   server: {
     host: '127.0.0.1',
     port: 5173,
     strictPort: true,
   },
-});
+}));
